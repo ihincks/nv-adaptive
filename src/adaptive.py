934885dc3a0f434ef41353232558a3f949115ae1
Ian <ian.hincks@gmail.com>
2017-12-20 09:29:37 -0500

    added some Pandas storage functions

--- src/adaptive.py
+++ src/adaptive.py
@@ -9,6 +9,7 @@ import numpy as np
 import models as m
 import datetime
 import dateutil
+from pandas import DataFrame, Panel, Timestamp, Timedelta
 
 
 #-------------------------------------------------------------------------------
@@ -22,12 +23,206 @@ SOME_PRIOR = qi.UniformDistribution(np.array([
             [1.5,3.5],
             [100**-1,1**-1]
         ]))
+        
+def get_now():
+    return Timestamp(datetime.datetime.now())
+        
+#-------------------------------------------------------------------------------
+# DATA STORAGE
+#-------------------------------------------------------------------------------
+
+def new_experiment_dataframe(updater):
+    """
+    Returns an empty `pandas.DataFrame` to store data from 
+    a single run of a single heuristic. Rows of this data 
+    structure correspond to single experimental configurations
+    on an NV center, summed over repetitions (n_meas). Columns
+    are as follows:
+    
+    `expparam`:         `expparam` object used by the updater's simulator, of 
+                        shape `(1,)`, and dtype `updater.model.expparam_dtype`.
+    `n_meas`:           The number of repetitions performed.
+    `bright`:           Number of bright reference photons measured, integer.
+    `dark`:             Number of dark reference photons measured, integer.
+    `signal`:           Number of signal reference photons measured, integer.
+    `completed_ts`:     Timestamp of when experiment was completed on the 
+                        physical hardware.
+    `returned_ts`:      Timestamp of when experiment was returned to python.
+    `submitted_ts`:     Timestamp of when experiment job was submitted.
+    `diff_time`:        Time between this `completed_ts` and the previous
+                        `completed_ts`.
+    `run_time`:         Actual experiment time, without server/hardware 
+                        overhead, in seconds
+    `wall_time`:        Time between job being requested, and result returned,
+                        in seconds.
+    `overhead_time`:    `e_wall_time` minus `e_run_time`.
+    `cum_wall_time`:    Cumulative time of this dataframe as measured by a 
+                        wall clock, marked by the `returned_ts` timestamps.
+    `cum_run_time`:     Cumulative time of actual experiment running.
+    `preceded_by_tracking`:     Whether this experiment was directly preceded by a 
+                        tracking operation, bool.
+    `eff_num_bits`:     Number of effective bits (strong measurments) 
+                        collected in this experiment.
+    `cum_eff_num_bits`: Cumulative number of effective bits collected in this
+                        dataframe.
+    `heuristic`:        Description of this heuristic.
+    `heuristic_value`:  Some kind of performance metric, as calculated prior to
+                        this experiment.
+    `smc_mean`:         Mean value of updater.
+    `smc_cov`:          Covariance matrix of updater.
+    `smc_n_eff_particles`:    Number of effective particles in updater.
+    `smc_resample_count`:     Number of resamples done by updater so far.
+    
+    :param qinfer.SMCUpdater updater: By convention, it is nice to store 
+        what's going on before any data have arrived.
+    """
+    df = DataFrame(
+        columns=[
+            'expparam',
+            'bright','dark','signal',
+            'completed_ts', 'returned_ts', 'submitted_ts',
+            'diff_time', 'run_time', 'wall_time', 'overhead_time', 
+            'cum_wall_time', 'cum_run_time',
+            'preceded_by_tracking',
+            'eff_num_bits', 'cum_eff_num_bits',
+            'heuristic', 'heuristic_value',
+            'smc_mean', 'smc_cov', 'smc_n_eff_particles', 'smc_resample_count'
+        ])
+    df = append_experiment_data(df, updater=updater, preceded_by_tracking=True)
+    
+    return df
+    
+def compute_run_time(expparam):
+    """
+    Computes the amount of time it takes to run all repetitions
+    of the given experiment back to back.
+    """
+    # this is the amount of time in AdaptiveTwoPulse.pp not
+    # spent on the experiment's pulse sequence, in microseconds
+    other_time = 27.65
+    
+    if expparam['emode'] == m.RabiRamseyModel.RABI:
+        pulse_time = expparam['t']
+    else:
+        pulse_time = 2 * expparam['t'] + expparam['tau']
+        
+    return float(1e-6 * expparam['n_meas'] * (pulse_time + other_time))
+    
+def compute_eff_num_bits(n_meas, updater):
+    """
+    Computes the number of effective number of strong measurements
+    given the current value of the references.
+    """
+    # hopefully hardcoding these indices doesn't come back to 
+    # haunt me
+    alpha = updater.particle_locations[:,5]
+    beta = updater.particle_locations[:,6]
+    n_eff = (alpha - beta)**2 / (5 * (alpha + beta))
+    return float(n_meas * np.dot(updater.particle_weights, n_eff))
+    
+
+def append_experiment_data(
+        dataframe, 
+        expparam=None, updater=None, 
+        result=None, job=None, 
+        preceded_by_tracking=False, heuristic_value=None
+    ):
+    """
+    Appends the results from a single experiment to a DataFrame, returning 
+    the new object.
+    
+    :param DataFrame dataframe: See `new_experiment_dataframe`.
+    :param np.ndarray expparam: Of dtype `updater.model.expparam_dtype`
+    :param ExperimentResult result: Result of the experiment. This is 
+        overwritted by job.get_result() if it exists. Normally parameter 
+        should not be input, and instead `job.get_result()` should be used.
+    :param AbstractExperimentJob job: Job that was run.
+    :param bool preceded_by_tracking: Whether or not we tracked directly prior to 
+        the experiment.
+    :param heuristic_value: Whatever we want to store here, depends on 
+        heuristic.
+    """
+    n_rows = dataframe.shape[0]
+    first_row = n_rows == 0
+    data = {'heuristic_value': heuristic_value}
+    
+    if updater is not None:
+        data['smc_mean'] = updater.est_mean()
+        data['smc_cov'] = updater.est_covariance_mtx()
+        data['smc_n_eff_particles'] = updater.n_ess
+        data['smc_resample_count'] = updater.resample_count
+        if first_row:
+            # this if statement will usually be invoked by the thing 
+            # initializing the data frame
+            data['eff_num_bits'] = 0
+            data['cum_eff_num_bits'] = 0
+            
+            data['run_time'] = Timedelta(0, 's')
+            data['cum_run_time'] = Timedelta(0, 's')
+            
+            data['wall_time'] = Timedelta(0, 's')
+            data['cum_wall_time'] = Timedelta(0, 's')
+            # these timestamps are a lie, but they remove a 
+            # allow the first row of the dataframe to be pre-data
+            now =  get_now()
+            data['submitted_ts'] = now
+            data['returned_ts'] = now
+            data['completed_ts'] = now
+            data['diff_time'] = Timedelta(0, 's')
+            data['overhead_time'] = Timedelta(0, 's')
+        else:
+            if expparam is not None:
+                data['eff_num_bits'] = compute_eff_num_bits(
+                    expparam['n_meas'], updater
+                )
+            prev_cenb = dataframe.loc[n_rows-1, 'cum_eff_num_bits']
+            data['cum_eff_num_bits'] = prev_cenb + data['eff_num_bits']
+    
+    if expparam is not None:
+        data['expparam'] = expparam
+        data['n_meas'] = expparam['n_meas']
+        data['run_time'] = Timedelta(compute_run_time(expparam), 's')
+        if first_row:
+            data['cum_run_time'] = data['run_time']
+        else:
+            prev_cum_run_time = dataframe.loc[n_rows-1, 'cum_run_time']
+            data['cum_run_time'] = prev_cum_run_time + data['run_time']
+    
+    if job is not None:
+        result = job.get_result()
+        data['submitted_ts'] = job.submitted_ts
+        data['returned_ts'] = job.returned_ts
+        data['wall_time'] = job.returned_ts - job.submitted_ts
+        data['overhead_time'] = data['wall_time'] - data['run_time']
+        if first_row:
+            data['cum_wall_time'] = data['wall_time']
+        else:
+            prev_return = dataframe.loc[n_rows-1, 'returned_ts']
+            prev_cum_wall_time = dataframe.loc[n_rows-1, 'cum_wall_time']
+            data['cum_wall_time'] = prev_cum_wall_time \
+                + job.returned_ts - prev_return
+
+    if result is not None:
+        data['bright'] = result.bright
+        data['dark'] = result.dark
+        data['signal'] = result.signal
+        data['preceded_by_tracking'] = result.preceded_by_tracking
+        data['completed_ts'] = result.timestamp
+        if first_row:
+            data['diff_time'] = Timedelta(0, 's')
+        else:
+            prev_timestamp = dataframe.loc[n_rows-1, 'completed_ts']
+            data['diff_time'] = result.timestamp - prev_timestamp
+        
+    return dataframe.append(data, ignore_index=True)
+        
+    
 
 #-------------------------------------------------------------------------------
 # SWEEP EXPPARAMS
 #-------------------------------------------------------------------------------
 
-def rabi_sweep(min_t=None, max_t=0.3, n=50, n_bin=None, wo=0):
+def rabi_sweep(min_t=None, max_t=0.3, n=50, n_meas=None, wo=0, mode=None):
     ham_model = m.RabiRamseyModel()
     if min_t is None:
         min_t = max_t / n
@@ -39,14 +234,16 @@ def rabi_sweep(min_t=None, max_t=0.3, n=50, n_bin=None, wo=0):
         np.ones(n) * ham_model.RABI
     ]
     dtype = ham_model.expparams_dtype
-    if n_bin is not None:
-        vals = vals + [(n_bin * np.ones(n)).astype(np.int)]
-        dtype = dtype + [('n_meas','int')]
+    if n_meas is not None:
+        mode = m.ReferencedPoissonModel.SIGNAL if mode is None else mode
+        vals = vals + [(mode * np.ones(n)).astype(np.int)]
+        vals = vals + [(n_meas * np.ones(n)).astype(np.int)]
+        dtype = dtype + [('mode', 'int'),('n_meas','int')]
     rabi_eps = np.array(vals).T
     rabi_eps = np.array(list(zip(*rabi_eps.T)), dtype=dtype)
     return rabi_eps
 
-def ramsey_sweep(min_tau=None, max_tau=2, tp=0.01, phi=0, n=50, n_bin=None, wo=0):
+def ramsey_sweep(min_tau=None, max_tau=2, tp=0.01, phi=0, n=50, n_meas=None, wo=0, mode=None):
     ham_model = m.RabiRamseyModel()
     if min_tau is None:
         min_tau = max_tau / n
@@ -58,9 +255,11 @@ def ramsey_sweep(min_tau=None, max_tau=2, tp=0.01, phi=0, n=50, n_bin=None, wo=0
         np.ones(n) * ham_model.RAMSEY
     ]
     dtype = ham_model.expparams_dtype
-    if n_bin is not None:
-        vals = vals + [(n_bin * np.ones(n)).astype(np.int)]
-        dtype = dtype + [('n_meas','int')]
+    if n_meas is not None:
+        mode = m.ReferencedPoissonModel.SIGNAL if mode is None else mode
+        vals = vals + [(mode * np.ones(n)).astype(np.int)]
+        vals = vals + [(n_meas * np.ones(n)).astype(np.int)]
+        dtype = dtype + [('mode', 'int'),('n_meas','int')]
     ramsey_eps = np.array(vals).T
     ramsey_eps = np.array(list(zip(*ramsey_eps.T)), dtype=dtype)
     return ramsey_eps
@@ -138,11 +337,12 @@ class NVDriftStepper(StochasticStepper):
         self._set_value()
 
 class ExperimentResult(object):
-    def __init__(self, bright, dark, signal, timestamp=None):
+    def __init__(self, bright, dark, signal, timestamp=None, preceded_by_tracking=False):
         self._bright = bright
         self._dark = dark
         self._signal = signal
-        self._timestamp = datetime.datetime.now() if timestamp is None else timestamp
+        self._preceded_by_tracking = preceded_by_tracking
+        self._timestamp = get_now() if timestamp is None else timestamp
         
     @property
     def bright(self):
@@ -154,13 +354,20 @@ class ExperimentResult(object):
     def signal(self):
         return self._signal
     @property
+    def preceded_by_tracking(self):
+        return self._preceded_by_tracking
+    @property
     def timestamp(self):
         return self._timestamp
     @property
     def triplet(self):
         return np.array([self.bright, self.dark, self.signal])
         
-class AbstractExperimentJob(with_metaclass(abc.ABCMeta, object)):
+class ExperimentJob(with_metaclass(abc.ABCMeta, object)):
+    def __init__(self):
+        self.submitted_ts = get_now()
+        self.returned_ts = None
+        
     @abc.abstractproperty
     def is_complete(self):
         pass
@@ -169,21 +376,28 @@ class AbstractExperimentJob(with_metaclass(abc.ABCMeta, object)):
     def get_result(self):
         if not self.is_complete:
             raise RuntimeError('Job is not complete.')
+        self.returned_ts = get_now()
             
-class OfflineExperimentJob(AbstractExperimentJob):
-    def __init__(self, result):
-        self._result = result
+class OfflineExperimentJob(ExperimentJob):
+    def __init__(self):
+        super(OfflineExperimentJob, self).__init__()
+        self._is_complete = False
     
     @property    
     def is_complete(self):
-        return True
+        return self._is_complete
+        
+    def push_result(self, bright, dark, signal):
+        self._result = ExperimentResult(bright, dark, signal)
+        self._is_complete = True
         
     def get_result(self):
         super(OfflineExperimentJob, self).get_result()
         return self._result
         
-class TopChefExperimentJob(AbstractExperimentJob):
+class TopChefExperimentJob(ExperimentJob):
     def __init__(self, topchef_job):
+        super(TopChefExperimentJob, self).__init__()
         self._job = topchef_job
     
     @property    
@@ -193,11 +407,22 @@ class TopChefExperimentJob(AbstractExperimentJob):
     def get_result(self):
         super(TopChefExperimentJob, self).get_result()
         result = self._job.result
+        ts = dateutil.parser.parse(result['time_completed'])
+        ts = Timestamp(ts.replace(tzinfo=None))
+        
+        preceded_by_tracking = False
+        bright = result['light_count']
+        dark = result['dark_count']
+        signal = result['result_count']
+        # our ghetto way of encoding tracks in the JSON result
+        if bright > 1e6:
+            bright, dark, signal = bright - 1e6, dark - 1e6, signal - 1e6
+            preceded_by_tracking = True
+            
         return ExperimentResult(
-            result['light_count'],
-            result['dark_count'],
-            result['result_count'],
-            dateutil.parser.parse(result['time_completed'])
+            bright, dark, signal, 
+            preceded_by_tracking=preceded_by_tracking, 
+            timestamp=ts
         )
 
 class AbstractRabiRamseyExperimentRunner(object):
@@ -217,7 +442,7 @@ class AbstractRabiRamseyExperimentRunner(object):
         return self._experiment_history
     
     @abc.abstractmethod
-    def run_experiment(self, expparam):
+    def run_experiment(self, expparam, precede_by_tracking=False):
         """
         Runs the experiment with the given expparams 
         and returns an instance of `AbstractExperimentJob`
@@ -234,7 +459,7 @@ def experiment_time(expparam):
     if expparam['emode'] == m.RabiRamseyModel.RABI:
         t_experiment = expparam['t']
     else:
-        t_experiment = expparam['tau'] + 2 * exparam['t']
+        t_experiment = expparam['tau'] + 2 * expparam['t']
     return expparam['mode'] * (t_experiment + extra_time_per_shot) \
             + extra_time_per_experiment
         
@@ -263,8 +488,9 @@ class SimulatedRabiRamseyExperimentRunner(AbstractRabiRamseyExperimentRunner):
     def run_tracking(self):
         self.drift.reset()
         
-    def run_experiment(self, expparam):
+    def run_experiment(self, expparam, precede_by_tracking=False):
         super(SimulatedRabiRamseyExperimentRunner, self).run_experiment(expparam)
+        job = OfflineExperimentJob()
         self.update_timestep(expparam)
         expparam['mode'] = m.ReferencedPoissonModel.BRIGHT
         bright = self._model.simulate_experiment(self.modelparam, expparam)
@@ -272,10 +498,8 @@ class SimulatedRabiRamseyExperimentRunner(AbstractRabiRamseyExperimentRunner):
         dark = self._model.simulate_experiment(self.modelparam, expparam)
         expparam['mode'] = m.ReferencedPoissonModel.SIGNAL
         signal = self._model.simulate_experiment(self.modelparam, expparam)
-        
-        return OfflineExperimentJob(
-            ExperimentResult(bright, dark, signal)
-        )
+        job.push_result(bright, dark, signal)
+        return job
         
 class RealRabiRamseyExperimentRunner(AbstractRabiRamseyExperimentRunner):
     def __init__(self, topchef_service):
@@ -312,10 +536,11 @@ class RealRabiRamseyExperimentRunner(AbstractRabiRamseyExperimentRunner):
            'pulse2_offset_freq': 0,
            'pulse2_modulation_freq': 0,
            'pulse2_modulation_phase': 0,
-           'adiabatic_power': self._adiabatic_power
+           'adiabatic_power': self._adiabatic_power,
+           'precede_by_tracking': False
        }
         
-    def run_experiment(self, expparam):
+    def run_experiment(self, expparam, precede_by_tracking=False):
         super(RealRabiRamseyExperimentRunner, self).run_experiment(expparam)
         
         eps = self._bare_eps
@@ -328,6 +553,9 @@ class RealRabiRamseyExperimentRunner(AbstractRabiRamseyExperimentRunner):
             eps['pulse2_time'] = 1e-6 * expparam['t']
             eps['pulse2_phase'] = expparam['phi']
         eps['center_freq'] = eps['center_freq'] - 1e6 * expparam['wo']
+        
+        if precede_by_tracking:
+            eps['precede_by_tracking'] = True
         
         # the JSON parser doesn't like getting np arrays
         for key in eps.keys():
@@ -409,10 +637,10 @@ class InfoGainHeuristic(qi.Heuristic):
         return eps
     
 class ExponentialHeuristic(qi.Heuristic):
-    def __init__(self, updater, max_t=0.3, max_tau=2, base=11/10, n=50, n_bin=100, name=None):
+    def __init__(self, updater, max_t=0.3, max_tau=2, base=11/10, n=50, n_meas=100, name=None):
         self.updater = updater
-        self._rabi_eps = rabi_sweep(max_t=1, n=n, n_bin=n_bin)
-        self._ramsey_eps = ramsey_sweep(max_tau=1, n=n, n_bin=n_bin)
+        self._rabi_eps = rabi_sweep(max_t=1, n=n, n_meas=n_meas)
+        self._ramsey_eps = ramsey_sweep(max_tau=1, n=n, n_meas=n_meas)
         
         self._rabi_eps['t'] = max_t * (base ** np.arange(n)) / (base ** (n-1))
         self._ramsey_eps['tau'] = max_tau * (base ** np.arange(n)) / (base ** (n-1))
@@ -433,10 +661,10 @@ class ExponentialHeuristic(qi.Heuristic):
         return eps
     
 class LinearHeuristic(qi.Heuristic):
-    def __init__(self, updater, max_t=0.3, max_tau=2, n=50, n_bin=100, name=None):
+    def __init__(self, updater, max_t=0.3, max_tau=2, n=50, n_meas=100, name=None):
         self.updater = updater
-        self._rabi_eps = rabi_sweep(max_t=1, n=n, n_bin=n_bin)
-        self._ramsey_eps = ramsey_sweep(max_tau=1, n=n, n_bin=n_bin)
+        self._rabi_eps = rabi_sweep(max_t=1, n=n, n_meas=n_meas)
+        self._ramsey_eps = ramsey_sweep(max_tau=1, n=n, n_meas=n_meas)
         
         self._rabi_eps['t'] = np.round(self._rabi_eps['t'] / 0.002) * 0.002
         self._ramsey_eps['tau'] = np.round(self._ramsey_eps['tau'] / 0.002) * 0.002
