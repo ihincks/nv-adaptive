c7deeeacbb19dc70dbf1f9b780f56a895518d28b
Ian Hincks <ian.hincks@gmail.com>
2017-12-20 15:18:45 -0500

    trying yet another tracking heuristic

--- src/adaptive.py
+++ src/adaptive.py
@@ -830,8 +830,7 @@ class TrackingHeuristic(qi.Heuristic):
         
     def _decide_on_tracking(self):
         bright_est = self.updater.est_mean()[5]
-        bright_std = self.updater.est_covariance_mtx()[5,5]
-        return bright_est + 1.5 * bright_std < self._initial_bright_mean - 1.5 * self._initial_bright_std
+        return bright_est < self._initial_bright_mean - 4 * self._initial_bright_std
         
         
     def __call__(self, tp):
